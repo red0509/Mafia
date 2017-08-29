d136d228af8cf1bcf60cb00494c7fc193d6804ca
Anton Pavlov <paapavlov@gmail.com>
2017-08-29 12:44:10 +0300

    2

--- Mafia/AppDelegate.swift
+++ Mafia/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
   
-        //change1
+        //change2
         return true
     }
 
